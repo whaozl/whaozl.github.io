4e3fb92a85613ff563312d256ebaf175b5df26c4
whaozl <whaozl@163.com>
2016-08-07 18:24:34 +0800

    6

--- public/js/base.js
+++ public/js/base.js
@@ -123,6 +123,6 @@ function pjax_loadDuoshuo(){
 function pjax_loadBUSUANZI(){
 	var busuanzi=$(".busuanzi");
 	if($(dus).length==1){
-		busuanzi.html("<span id="busuanzi_container_page_pv"> 本文总阅读量<span id="busuanzi_value_page_pv"></span>次</span>");
+		busuanzi.html("<span id='busuanzi_container_page_pv'> 本文总阅读量<span id='busuanzi_value_page_pv'></span>次</span>");
 	}
 }
